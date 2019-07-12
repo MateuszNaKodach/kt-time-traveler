9dc1fe6c97c5646029dc47545aeed38c4694ae51
MateuszNowak <kontakt.mateusznowak@gmail.com>
2019-07-12 19:27:07 +0200

    #2 Change library version to 0.0.1

--- kt-time-traveler-core/build.gradle.kts
+++ kt-time-traveler-core/build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
 }
 
 group = "com.github.nowakprojects"
-version = "1.0.0-SNAPSHOT"
+version = "0.0.1"
 
 repositories {
     mavenCentral()
--- kt-time-traveler-spring-boot-starter/build.gradle.kts
+++ kt-time-traveler-spring-boot-starter/build.gradle.kts
@@ -13,7 +13,7 @@ plugins {
 }
 
 group = "com.github.nowakprojects"
-version = "1.0.0-SNAPSHOT"
+version = "0.0.1"
 java.sourceCompatibility = JavaVersion.VERSION_1_8
 
 configurations {
