548a4cedab6ba325900a923578a71ec6f3eb97ac
Mateusz Nowak <mateusz.nowak@vonage.com>
2020-07-01 12:44:17 +0200

    #6 Enable tests in GitHub actions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("jvm") version "1.3.41"
-    kotlin("plugin.spring") version "1.3.41"
+    kotlin("jvm") version "1.3.70"
+    kotlin("plugin.spring") version "1.3.70"
     id("io.spring.dependency-management") version "1.0.7.RELEASE"
     id("maven")
 }
